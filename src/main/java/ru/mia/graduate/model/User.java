420baa5cc6b447ec8461a729c38efcd7825fc13e
Mironov Jury <mironoff2007@rambler.ru>
2018-08-04 00:50:43 +0300

    User and Roles classes added

--- src/main/java/ru/mia/graduate/model/User.java
+++ src/main/java/ru/mia/graduate/model/User.java
@@ -1,9 +1,53 @@
 package ru.mia.graduate.model;
 
 
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
 
 public class User {
-
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
     int id;
-    String userRole;
+
+    @Column(name="name")
+    String name;
+
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        //this.roles = CollectionUtils.isEmpty(roles) ? Collections.emptySet() : EnumSet.copyOf(roles);
+    }
+    public Set<Role> getRoles() {
+        return roles;
+    }
+    public User(String name, Collection<Role> roles) {
+
+        this.name =name;
+        setRoles(roles);
+    }
 }
